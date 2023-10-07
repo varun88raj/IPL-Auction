0def827bf3efc6fb9d3af6fdf2837f89cb971d3b
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 18:29:53 +0530

    Update allrounder.cpp
    chr* to string

--- 19pt34_oops/allrounder.cpp
+++ 19pt34_oops/allrounder.cpp
@@ -9,9 +9,9 @@ void allrounder::printData() const
         cout<<setw(4)<<player_id<<setw(20)<<name<<setw(4)<<matches<<setw(6)<<runs<<setw(8)<<batting_average<<setw(8)<<batting_strike_rate<<setw(6)<<_50s<<setw(6)<<_100s<<setw(6)<<best_score<<setw(6)<<wickets<<setw(8)<<bowling_average<<setw(8)<<bowling_strike_rate<<setw(8)<<economy<<setw(4)<<_4_wicket_haul<<setw(4)<<_5_wicket_haul<<setw(6)<<best.runs_given<<"/"<<best.wickets_taken
         <<setw(15)<<s->position<<setw(15)<<s->type<<setw(10)<<s->bowl_type<<setw(8)<<s->batting_hand<<setw(8)<<s->batting_hand<<setw(15)<<c->Country<<setw(10)<<c->base_price<<"\n";
 }
-void allrounder::Ar_retrieveData(char n[20],int m,int p,int r,float batavg,float batstr,int fif,int hun,int b,int w,float bowavg,float bowstr,float eco,int _4w,int _5w,int bestrun,int bestwic,char co[20],char typ[20],char bowtyp[20],char boh[20],char bah[20],char pos[20],int base)
+void allrounder::Ar_retrieveData(string n,int m,int p,int r,float batavg,float batstr,int fif,int hun,int b,int w,float bowavg,float bowstr,float eco,int _4w,int _5w,int bestrun,int bestwic,string co,string typ,string bowtyp,string boh,string bah,string pos,int base) const
 {
-        strcpy(name,n);
+        name=n;
         matches=m;
         player_id=p;
         runs=r;
@@ -28,12 +28,12 @@ void allrounder::Ar_retrieveData(char n[20],int m,int p,int r,float batavg,float
         _5_wicket_haul=_5w;
         best.runs_given=bestrun;
         best.wickets_taken=bestwic;
-        strcpy(c->Country,co);
-        strcpy(s->type,typ);
-        strcpy(s->bowl_type,bowtyp);
-        strcpy(s->bowling_hand,boh);
-        strcpy(s->batting_hand,bah);
-        strcpy(s->position,pos);
+        c->Country=co;
+        s->type=typ;
+        s->bowl_type=bowtyp;
+        s->bowling_hand=boh;
+        s->batting_hand=bah;
+        s->position=pos;
         c->base_price=base;
 }
 void allrounder::Ar_filterData(int m,int r,float batavg,float batstr,int w,float bowavg,float bowstr,float eco) const
@@ -49,7 +49,7 @@ int allrounder::Ar_base() const
 }
 int allrounder::Ar_foreign_player() const
 {
-        if(strcmp(c->Country,"Foreigner")==0)
+        if(c->Country=="Foreigner")
         {
                 return 1;
         }
