8dbe64e1a1a13a14b5e4b99e440707e9276e2579
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 14:56:41 +0530

    Update player.h

--- 19pt34_oops/player.h
+++ 19pt34_oops/player.h
@@ -1,15 +1,16 @@
 #include "style.h"
 #include "country.h"
+#include <memory>
 #ifndef player_h
 #define player_h
 class player
 {
 private:
-        int player_id;
+        int player_id=0;
         char name[20];
-        int matches;
-        country*c;
-        style*s;
+        int matches=0;
+        std::unique_ptr<country> c;
+        std::unique_ptr<style> s;
 public:
         player();
         virtual void setData();
