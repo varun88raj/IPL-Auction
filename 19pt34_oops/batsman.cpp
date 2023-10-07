018cb48267205e13db72d1bac01c76e1f9219ea0
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 14:34:41 +0530

    Update batsman.cpp
    Making constructor with intialization list rather thaan assigning values

--- 19pt34_oops/batsman.cpp
+++ 19pt34_oops/batsman.cpp
@@ -1,13 +1,6 @@
 #include "batsman.h"
-batsman::batsman()
-{
-        runs=0;
-        batting_average=0;
-        batting_strike_rate=0;
-        _50s=0;
-        _100s=0;
-        best_score=0;
-}
+batsman::batsman():runs(0),batting_average(0),batting_strike_rate(0),_50s(0),_100s(0),best_score(0){}
+
 void batsman::setData()
 {
         player::setData();
