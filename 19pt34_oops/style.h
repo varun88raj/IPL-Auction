e15cfb3ab96f7ba9faa260bb7893f94d7555fedd
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 13:23:30 +0530

    Update style.h

--- 19pt34_oops/style.h
+++ 19pt34_oops/style.h
@@ -2,12 +2,13 @@
 #define style_h
 class style
 {
-public:
+private:
         char position[20];
         char type[20];
         char batting_hand[10];
         char bowling_hand[10];
         char bowl_type[10];
+public:
         style();
         void SetData();
         char getPosition();
