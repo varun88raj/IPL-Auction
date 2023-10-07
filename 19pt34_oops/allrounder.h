e94826fba2907da0aacdfa2bab4e371074846792
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 22:18:56 +0530

    Update allrounder.h

--- 19pt34_oops/allrounder.h
+++ 19pt34_oops/allrounder.h
@@ -5,7 +5,7 @@ class allrounder:public batsman,public bowler
 public:
         void setAllRound();
         void printAllRound()const;
-        void retrieveAllRound(std::string&,int,int,int,float,float,int,int,int,int,float,float,float,int,int,int,int,std::string&,std::string&,std::string&,std::string&,std::string&,std::string&,int);
+        void retrieveAllRound(std::string_view,int,int,int,float,float,int,int,int,int,float,float,float,int,int,int,int,std::string_view,std::string_view,std::string_view,std::string_view,std::string_view,std::string_view,int);
         void filterAllRound(int,int,float,float,int,float,float,float) const;
         int base_of_AllRound() const;
         int foreign_player_AllRound() const;
