d28cbca3e6fd3aa52efd73e64345a9cbeafb8cd2
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 21:26:17 +0530

    Update style.h
    No Using namespace std in header file

--- 19pt34_oops/style.h
+++ 19pt34_oops/style.h
@@ -3,15 +3,14 @@
 #include <string>
 #include <iostream>
 
-using namespace std;
 class style
 {
 public:
-        string position="position";
-        string type="type";
-        string batting_hand="bat";
-        string bowling_hand="bowl";
-        string bowl_type="bowl_type";
+        std::string position="position";
+        std::string type="type";
+        std::string batting_hand="bat";
+        std::string bowling_hand="bowl";
+        std::string bowl_type="bowl_type";
         void SetData();
         char getPosition() const;
         char getType() const;
