8b6ab28195880604462ad26e9de0363847126f95
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 15:27:55 +0530

    Update allrounder.h
    rename functions

--- 19pt34_oops/allrounder.h
+++ 19pt34_oops/allrounder.h
@@ -5,8 +5,8 @@ class allrounder:public batsman,public bowler
 public:
         void setData() override;
         void printData() override;
-        void retrieveData(char*,int,int,int,float,float,int,int,int,int,float,float,float,int,int,int,int,char*,char*,char*,char*,char*,char*,int);
-        void filterData(int,int,float,float,int,float,float,float);
-        int base();
-        int foreign_player();
+        void Ar_retrieveData(char*,int,int,int,float,float,int,int,int,int,float,float,float,int,int,int,int,char*,char*,char*,char*,char*,char*,int);
+        void Ar_filterData(int,int,float,float,int,float,float,float);
+        int Ar_base();
+        int Ar_foreign_player();
 };
