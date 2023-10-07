e821b76e1fd3c35f63d1d248947c650d4eac51ff
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 18:18:59 +0530

    Update player.h
    chr* to string

--- 19pt34_oops/player.h
+++ 19pt34_oops/player.h
@@ -1,19 +1,21 @@
 #include "style.h"
 #include "country.h"
 #include <memory>
+#include <stdlib.h>
 #ifndef player_h
 #define player_h
+
 class player
 {
-private:
+protected:
         int player_id=0;
-        char name[20];
+        string name;
         int matches=0;
         std::unique_ptr<country> c;
         std::unique_ptr<style> s;
 public:
         player();
         virtual void setData();
-        virtual void printData();
+        virtual void printData() const;
 };
 #endif
