61ec51456b1cac38b48d42f6ddbe7ea75241f3e2
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 15:02:18 +0530

    Update team_details.h
    inclass intialization

--- 19pt34_oops/team_details.h
+++ 19pt34_oops/team_details.h
@@ -1,15 +1,14 @@
 class details
 {
 private:
-        int no_of_players;
-        int no_of_foreign_players;
-        long long int purse;
-        int no_of_batsmans;
-        int no_of_bowlers;
-        int no_of_All_rounders;
-        int no_of_wicketkeepers;
+        int no_of_players = 0;
+        int no_of_foreign_players=0;
+        long long int purse=0;
+        int no_of_batsmans=0;
+        int no_of_bowlers=0;
+        int no_of_All_rounders=0;
+        int no_of_wicketkeepers=0;
 public:
-        details();
         details(int,int,long int,int,int,int,int);
         void display();
 };
