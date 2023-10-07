8e790d91a2416e5448208788475425577c3bf3f3
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 18:11:58 +0530

    Update team_details.h
    Error fix

--- 19pt34_oops/team_details.h
+++ 19pt34_oops/team_details.h
@@ -1,6 +1,6 @@
 class details
 {
-private:
+public:
         int no_of_players = 0;
         int no_of_foreign_players=0;
         long long int purse=0;
@@ -8,7 +8,6 @@ private:
         int no_of_bowlers=0;
         int no_of_All_rounders=0;
         int no_of_wicketkeepers=0;
-public:
         setdetails(int,int,long int,int,int,int,int);
         void display();
 };
