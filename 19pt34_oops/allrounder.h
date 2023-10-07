9757bcb3538bde21db4fe6324f12b33b01afb546
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 15:20:33 +0530

    Update allrounder.h
    annotate with override

--- 19pt34_oops/allrounder.h
+++ 19pt34_oops/allrounder.h
@@ -3,8 +3,8 @@
 class allrounder:public batsman,public bowler
 {
 public:
-        void setData();
-        void printData();
+        void setData() override;
+        void printData() override;
         void retrieveData(char*,int,int,int,float,float,int,int,int,int,float,float,float,int,int,int,int,char*,char*,char*,char*,char*,char*,int);
         void filterData(int,int,float,float,int,float,float,float);
         int base();
