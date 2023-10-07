fa3b1f7e2ac1b240456ff945353592becde02d61
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-07 21:55:50 +0530

    Update batsman.h
    pass by reference

--- 19pt34_oops/batsman.h
+++ 19pt34_oops/batsman.h
@@ -13,7 +13,7 @@ protected:
 public:
         void setBatsman();
         void printBatsman() const;
-        void retrieveBatsman(std::string,int,int,int,float,float,int,int,int,std::string,std::string,std::string,std::string,std::string,std::string,int);
+        void retrieveBatsman(std::string&,int,int,int,float,float,int,int,int,std::string&,std::string&,std::string&,std::string&,std::string&,std::string&,int);
         void filterBatsman(int,int,float,float) const;
         int base_of_bat() const;
         int foreign_player_of_bat() const;
