2e874fa439f98b0ac0dfd23d873d26c29219a475
varun88raj <74340962+varun88raj@users.noreply.github.com>
2023-10-10 22:29:40 +0530

    Update team_details.h
    adding team name

--- 19pt34_oops/team_details.h
+++ 19pt34_oops/team_details.h
@@ -1,6 +1,8 @@
+#include <string>
 class details
 {
 public:
+        std::string team_name = "name";
         int no_of_players = 0;
         int no_of_foreign_players=0;
         long long int purse=0;
@@ -8,7 +10,9 @@ public:
         int no_of_bowlers=0;
         int no_of_All_rounders=0;
         int no_of_wicketkeepers=0;
-        void setdetails(int,int,long int,int,int,int,int);
+        details(const std::string& name, int players, int foreign_players,
+        long long int team_purse, int batsmans, int bowlers,
+        int all_rounders, int wicketkeepers);
         void display() const;
 };
 
